0dc48b2999504eefbb5c7123c3e4201847e70f1d
Kieran <kieran.webdevelopment@gmail.com>
2022-06-22 16:49:10 +0100

    end of day wednesday

--- study-budz-2.0/src/Components/Form/index.js
+++ study-budz-2.0/src/Components/Form/index.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import './Form.css';
 
 function Form() {
-  const [fullName, setFullName] = useState('');
+  const [name, setName] = useState('');
   const [topic, setTopic] = useState('');
-  const [describe, setDescribe] = useState('');
-  const [meetingLink, setMeetingLink] = useState('');
-  const [meetingLength, setMeetingLength] = useState('');
-  const [sessionTime, setSessionTime] = useState('');
-  const [paypalLink, setPaypalLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [zoom, setZoom] = useState('');
+  const [timeduration, setTimeDuration] = useState('');
+  const [datetime, setDateTime] = useState('');
+  const [paypalemail, setPaypalEmail] = useState('');
 
   // fname: 'fname',
   // lname: 'lname',
@@ -19,49 +19,32 @@ function Form() {
   // intrests: 'interest',
 
   async function submitForm(e) {
-    e.preventDefault();
+    // e.preventDefault();
+    const duration = Number(timeduration);
+
     const studySession = {
-      fullName,
+      name,
       topic,
-      describe,
-      meetingLink,
-      sessionTime,
-      meetingLength,
-      paypalLink,
+      description,
+      zoom,
+      datetime,
+      duration,
+      paypalemail,
     };
+
     console.log(studySession);
 
-    const post = await fetch('https://studybudzapp.herokuapp.com/lessons', {
+    const post = await fetch('http://localhost:4000/lessons', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        name: 'fullName',
-        topic: 'topic',
-        description: 'describe',
-        zoom: 'meetingLink',
-        paypalemail: 'paypalLink',
-        datetime: 'sessionTime',
-        duration: 'meetingLength',
-      }),
+      body: JSON.stringify(studySession),
     });
     const data = await post.json();
     console.log(data);
-
-    setFullName('');
-    setTopic('');
-    setDescribe('');
-    setSessionTime('');
-    setMeetingLink('');
-    setMeetingLength('');
   }
 
   function clearForm(e) {
     e.preventDefault();
-    setFullName('');
-    setTopic('');
-    setDescribe('');
-    setMeetingLink('');
-    setMeetingLength('');
   }
 
   return (
@@ -72,8 +55,8 @@ function Form() {
           type="text"
           placeholder="Bob Smith"
           required
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         ></input>
         <label>What topics are you teaching?</label>
         <input
@@ -88,42 +71,42 @@ function Form() {
           type="text"
           placeholder="I like reading and building stuff"
           required
-          value={describe}
-          onChange={(e) => setDescribe(e.target.value)}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         ></input>
         <label>Meeting Link</label>
         <input
           type="text"
           placeholder="www.zoom.com"
           required
-          value={meetingLink}
-          onChange={(e) => setMeetingLink(e.target.value)}
+          value={zoom}
+          onChange={(e) => setZoom(e.target.value)}
         ></input>
         <label>Paypal Email</label>
         <input
           type="text"
           placeholder="www.paypal.com"
           required
-          value={paypalLink}
-          onChange={(e) => setPaypalLink(e.target.value)}
+          value={paypalemail}
+          onChange={(e) => setPaypalEmail(e.target.value)}
         ></input>
         <label>Choose a date and time for your session</label>
         <input
           type="datetime-local"
-          value={sessionTime}
+          value={datetime}
           min="2022-07-21T00:00"
           max="2023-07-21T00:00"
-          onChange={(e) => setSessionTime(e.target.value)}
+          onChange={(e) => setDateTime(e.target.value)}
         ></input>
         <label>Meeting Length</label>
         <select
-          value={meetingLength}
-          onChange={(e) => setMeetingLength(e.target.value)}
+          value={timeduration}
+          onChange={(e) => setTimeDuration(e.target.value)}
         >
-          <option value="30 minutes">30 minutes</option>
-          <option value="60 minutes">60 minutes</option>
-          <option value="90 minutes">90 minutes</option>
-          <option value="more than 90 minutes">more than 90 minutes</option>
+          <option value="30">30 minutes</option>
+          <option value="60">60 minutes</option>
+          <option value="90">90 minutes</option>
+          <option value="120">more than 90 minutes</option>
         </select>
         <button className="submit" onClick={submitForm}>
           Submit
