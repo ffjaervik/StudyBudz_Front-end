0d76572cd5912668dd0dbe3d12bdd136bd6277bc
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-06-23 15:34:08 +0100

    Added content to HowToWorkTogether component

--- study-budz-2.0/src/Components/HowToWorkTogether/index.js
+++ study-budz-2.0/src/Components/HowToWorkTogether/index.js
@@ -1,9 +1,20 @@
 const HowToWorkTogether = () => {
     return ( 
-        <div className="how-to-work-together">
-            <h1> How To Use Zoom</h1>
-            <p> Here I am teaching you how to properly use Zoom and the advantages it brings when using Zoom in teams. It will boost your productivity and allows you to meet more people online while fortering relations among people you've never met before.</p>
-        </div>
+        <>
+            <div className="how-to-work-together">
+                <h1> How To Work Together</h1>
+                <p> Here I am teaching you how to properly use Zoom and the advantages it brings when using Zoom in teams. It will boost your productivity and allows you to meet more people online while fortering relations among people you've never met before.</p>
+                <h2>How to use Zoom</h2>
+                <p>Follow the instruction on the video to install and get started on Zoom:</p>
+                    <ul>
+                        <li>Go to <a href= "https://zoom.us/">Zoom</a> and download the app (it will be useful for repeated use) </li>
+                        <li>Create an account and log in</li>
+                        <li>Either create a room (if you want to link it to one of your lessons) or copy the link of one lesson you's like to take part in </li>
+                        <li>Join a session and enjoy the flood of knowledge</li>
+                    </ul>
+                
+            </div>
+        </>
      );
 }
  
